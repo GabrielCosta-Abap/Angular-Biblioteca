9ea8b898cd544447212872534cfd5ac99aa9435e
Luis Henrique Ries <prof.ries@gmail.com>
2024-04-26 01:41:37 -0300

    CRUD de produtos acessando APIs

---
+++ src/app/tabela-produtos/tabela-produtos.component.ts
@@ -0,0 +1,35 @@
+import { Component, Input } from '@angular/core';
+import { Produto } from '../produto';
+import { ProdutoApiService } from '../produto-api.service';
+
+@Component({
+  selector: 'app-tabela-produtos',
+  templateUrl: './tabela-produtos.component.html',
+  styleUrl: './tabela-produtos.component.css'
+})
+export class TabelaProdutosComponent {
+  @Input() title= "Tabela de Produtos";
+  listaProdutos: Produto[] = [];
+  nomePesquisado= "";
+
+  constructor(private produtoApiService: ProdutoApiService) {    
+      this.listar();
+  }
+
+  listar() {
+    this.produtoApiService.listar().subscribe(
+      (produtos) => {
+        this.listaProdutos = produtos;
+      }
+    );
+  }
+
+  deletar(id?:number) {
+    this.produtoApiService.deletar(id!).subscribe(
+      (produto) => {
+        alert(`Produto deletado com sucesso!`);   
+        this.listar();     
+      }
+    )
+  }
+}
